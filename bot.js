c684634677d937caf5e18ae8bdc527890eaa1634
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-11-16 22:59:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./auth.json');
 var fs = require('fs');
-var user_data = require('./user_data.json');
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -12,7 +10,7 @@ logger.add(new logger.transports.Console, {
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
+   token: process.env.token,
    autorun: true
 });
 
@@ -33,21 +31,13 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 			money: 0,
 			karma: 0,
 			pets: [0, 0, 0],
-			totalPets: [0, 0, 0]
-		}
 
-		user_data[userID] = userDataz;
-
-		fs.writeFile('./user_data.json', JSON.stringify(user_data, null, 4), (err) => {
-			// good to go if no err(or)
-		});
-	}
 	
-    if (message.substring(0, 1) == '?') {
-        var args = message.substring(1).split(' ');
+    if (message.substring(0, 2) == 'yo') {
+	message = message.trim()
+        var args = message.substring(2).split(' ');
         var cmd = args[0];
-		
-       
+			
         args = args.splice(1);
         switch(cmd) {
             // !ping
@@ -57,35 +47,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     message: 'Pong!'
                 });
             break;
-				case 'BEG':
-				if (user_data[userID] != undefined){
-						bot.sendMessage({
-							to: channelID,
-							message: user + ', your begging has been answered. Your lead count has increased by 1'
-						});
-					user_data[userID]['money'] = user_data[userID]['money'] + 1;
-
-					fs.writeFile('./user_data.json', JSON.stringify(user_data, null, 4), (err) => {
-						// good to go if no err(or)
-					});
-				}
-				break;
-				case 'LEAD':
-					if (user_data[userID] != undefined){
-						bot.sendMessage({
-							to: channelID,
-							message: user + ', you currently have ' + user_data[userID]['money'] + ' lead!'
-						});
-					}
-					break;
-			case 'ORANGE':
-				bot.sendMessage({
-					to: channelID,
-					message: '!fire'
-				});
-			break;
-			
-            // Just add any case commands if you want to..
          }
      }
 });
