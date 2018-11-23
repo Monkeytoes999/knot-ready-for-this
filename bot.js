074ac701eceea84be81c1bd5ce32236f208f7948
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-11-23 13:38:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -113,7 +113,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 			    bot.addReaction({
 				channelID: channelID,
 				messageID: res.id,
-				reaction: 🤾‍♀️
+				reaction: '🤾‍♀️'
 			    });
 			});
 		break;
