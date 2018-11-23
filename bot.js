a30e029088329702d168820d7a64c152929f7ee0
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-11-22 22:24:40 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,10 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch(cmd) {
             // !ping
 		case 'STATUS':
+			console.log(message)
+			console.log(message.charCodeAt(6))
 			if ((message.charCodeAt(6) > 48 && message.charCodeAt(6) < 52) && (userID == '393586279964475393' || userID == '486985623161274378')){
+				console.log(colorDot[message.substring(6, 7)])
 				bot.setPresence({
 				    status: colorDot[message.substring(6, 7)]
 				});
