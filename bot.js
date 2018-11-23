8dc35d94871ac3335cb284eaf3bc7ab4c3cc3066
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-11-22 22:21:14 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var fs = require('fs');
+var colorDot = ['online', 'idle', 'dnd'];
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -24,7 +25,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
 	
-	if (message.substring(0, 2) == 'yo') {
+	message = message.toUpperCase();
+	
+	if (message.substring(0, 2) == 'YO') {
 	message = message.substring(2).trim()
         var args = message.split(' ');
         var cmd = args[0];
@@ -32,10 +35,17 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         args = args.splice(1);
         switch(cmd) {
             // !ping
-            case 'Ping':
+		case 'STATUS':
+			if ((message.charCodeAt(7) > 48 && message.charCodeAt(7) < 52) && (userID == '393586279964475393' || userID == '486985623161274378')){
+				bot.setPresence({
+				    status: colorDot[message.substring(7, 8)]
+				});
+			}
+		break;
+            case 'PING':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'pong!'
+                    message: 'Pong!'
                 });
             break;
          }
