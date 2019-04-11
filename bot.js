8cfbb6889dca158086fa733d9b10dd7ed722dfd0
Iceman013 <45405964+Iceman013@users.noreply.github.com>
2019-04-10 20:45:18 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -227,16 +227,15 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				}
 			}
 			var roleSearch = 0;
-			var finalRole = 0;
+			var finalRole = -1;
 			while (roleSearch < levels.length){
-				if (roll[roleSearch] == topRoleID){
-					finalRole = roleSearch + 1;
+				if (roll[roleSearch].equals(topRoleID)){
+					finalRole = roleSearch;
 				}
 				roleSearch = roleSearch + 1;
 			}
-			if (finalRole > 0){
+			if (finalRole >= 0){
 				console.log('Detecting top role of ' + (finalRole));
-				finalRole = finalRole - 1;
 				var outa = ['a'];
 				var outb = ['b'];
 				var outc = ['c'];
